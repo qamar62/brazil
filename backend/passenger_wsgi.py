42aa985f892a6bc9d8906e4cf3cbb3ba4acea3e0
qamar62 <qam600@gmail.com>
2024-12-30 11:13:07 +0400

    Update passenger_wsgi.py

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -2,12 +2,12 @@ import os
 import sys
 
 # Add your virtualenv Python path
-VIRTUALENV = "/home/yy89pc8fin0s/virtualenv/travel_operations/backend/3.9/lib/python3.9/site-packages"
+VIRTUALENV = "/home/yy89pc8fin0s/virtualenv/brazil/backend/3.9/lib/python3.9/site-packages"
 if VIRTUALENV not in sys.path:
     sys.path.insert(0, VIRTUALENV)
 
 # Add the project directory to the Python path
-project_dir = "/home/yy89pc8fin0s/travel_operations/backend"
+project_dir = "/home/yy89pc8fin0s/brazil/backend"
 if project_dir not in sys.path:
     sys.path.insert(0, project_dir)
 
